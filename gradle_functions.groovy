38e178614bb4469b601e0b2988a5cba6f9247515
Prajwal Gowda <pgowda@itemis.de>
2019-01-13 18:47:19 +0100

    Update gradle_functions.groovy

--- gradle_functions.groovy
+++ gradle_functions.groovy
@@ -1,10 +1,10 @@
 
 
-def gradleVersionUpdate(path,xtext_version){
+def gradleVersionUpdate(path,xtext_version, snapshot_version){
   def update_cmd
     dir(path) {
         update_cmd = sh (
-            script: "sed -i -e \"s/version = '${xtext_version}-SNAPSHOT'/version = '${xtext_version}'/g\" gradle/versions.gradle",
+            script: "sed -i -e \"s/version = '${snapshot_version}'/version = '${xtext_version}'/g\" gradle/versions.gradle",
             returnStdout: true
         ).trim()
     }
