a03e58c4e3eec1d874e8d13e523a05d61ed58529
Prajwal Gowda <pgowda@itemis.de>
2019-01-14 11:48:17 +0100

    Create add_upstreamjob.groovy

---
+++ add_upstreamjob.groovy
@@ -0,0 +1,17 @@
+
+def addUpstream(){
+   def insertstring="pipelineTriggers([upstream(threshold: \'SUCCESS\',upstreamProjects: \'xtext-lib/\' + URLEncoder.encode(\"BRANCH_NAME\", \"UTF-8\"))])"
+   filename="test_jenkinsfile"
+	 File fh = new File("${workspace}//test_jenkinsfile")
+   def linenum=0
+   def lines = fh.readLines()
+   for (line in lines) {
+	    linenum++
+	    if (line=~/BuildDiscarderProperty/){
+		    lines.add(linenum, insertstring)
+		   }
+    }
+  }
+  
+ retrun true
+       
