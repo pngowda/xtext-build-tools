d5e3dd0716e272a9200a8a030ece459d224cc09f
Prajwal Gowda <pgowda@itemis.de>
2019-02-03 23:32:25 +0100

    Update add_upstreamjob.groovy

--- add_upstreamjob.groovy
+++ add_upstreamjob.groovy
@@ -5,16 +5,17 @@ def addUpstream(upstreamJob){
    println insertTrigger
    File fh = new File("${workspace}/test_jenkinsfile")
    def linenum=0
-   //def lines = fh.readLines()
+   def lines = fh.readLines()
    //println lines
    LineNumberReader reader = fh.newReader()
-   while ((line = reader.readLine()) != null) {
+   for (line in lines){
+   //while ((line = reader.readLine()) != null) {
      linenum++
      println line
      if (line=~/^\s+]/){
        println line
        println linenum
-       //lines.add(linenum-1, insertstring)
+       lines.add(linenum-1, insertstring)
      }
    }   
    def w = fh.newWriter() 
