94c5aadf432c14c87693d1f287a0cd272db064f0
Prajwal Gowda <pgowda@itemis.de>
2019-02-07 15:48:55 +0100

    Update add_upstreamjob.groovy

--- add_upstreamjob.groovy
+++ add_upstreamjob.groovy
@@ -10,7 +10,7 @@ def addUpstream(upstreamJob, branchName){
    def linesW = fh.readLines()
    for (line in linesR){
      linenum++
-     (line=~ /^\s+pipelineTriggers\((.*)\)/).each {match -> println match }
+     (line=~ /^\s+pipelineTriggers\((.*)\)/).each {match -> println match[0] }
      if (line=~/^\s+]\)/){
        count++
        insertLineNumber=linenum
