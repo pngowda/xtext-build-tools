0d0c71dbf6e7f59836780d1a127b49f9d8673c30
Prajwal Gowda <pgowda@itemis.de>
2019-02-08 14:01:42 +0100

    Update add_upstreamjob.groovy

--- add_upstreamjob.groovy
+++ add_upstreamjob.groovy
@@ -2,37 +2,36 @@
 def addUpstream(upstreamJob, branchName){
    def insertTrigger=", pipelineTriggers([upstream(threshold: \'SUCCESS\', upstreamProjects: \'$upstreamJob/\' + URLEncoder.encode(\"$branchName\", \"UTF-8\"))])"
    def appendTrigger="[upstream(threshold: \'SUCCESS\', upstreamProjects: \'$upstreamJob/\' + URLEncoder.encode(\"$branchName\", \"UTF-8\"))]"
-   //println insertTrigger
    File fh = new File("${workspace}/test_jenkinsfile")
    def linenum=0
    def lineToReplace
-   def count=0
-   def insertLineNumber=0
-   def insertLineNumber1=0
+   def insert_new=0
+   def insert_append=0
    def linesR = fh.readLines()
    def linesW = fh.readLines()
    def exisitngTrigger
    for (line in linesR){
      linenum++
+     insert_new++
+     insert_append++
      if(line=~ /^\s+pipelineTriggers\((.*)\)/){
-        insertLineNumber1=linenum
-        lineToReplace=line
+        //insert_append=linenum
+        //lineToReplace=line
         (line=~ /^\s+pipelineTriggers\((.*)\)/).each {match -> exisitngTrigger=match[1] }
+        continue;
      }      
      if (line=~/^\s+]\)/){
-       count++
-       insertLineNumber=linenum
+       //insert_new=linenum
+       continue;
      }
    }
-   println count
-   println insertLineNumber1
-   println insertLineNumber
-   println "Trigger to append "+ exisitngTrigger+ ", " +appendTrigger
-   insertTrigger="pipelineTriggers(${exisitngTrigger}, ${appendTrigger})"
-   println "Trigger to append "+ insertTrigger
-   //linesW.add(insertLineNumber-1, "\t\t"+insertTrigger)
-   //linesW.drop(insertLineNumber1)
-   linesW.set(insertLineNumber1-1, "\t\t"+insertTrigger)
+   if(${exisitngTrigger}){
+      insertTrigger="pipelineTriggers(${exisitngTrigger}, ${appendTrigger})"
+      linesW.set(insert_append-1, "\t\t"+insertTrigger)
+   }
+   else{
+      linesW.add(insert_new-1, "\t\t"+insertTrigger)
+   }
    def w = fh.newWriter() 
    for(wline in linesW){
        w<< wline +"\n"
