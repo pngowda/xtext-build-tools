6d016c27060604bc60ef591a61cefc78be71d7b3
Karsten Thoms <karsten.thoms@itemis.de>
2019-03-06 13:44:35 +0100

    Write JUnit report from results
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- version_functions.groovy
+++ version_functions.groovy
@@ -2,12 +2,20 @@ def getLatestArtifactVersion(groupId, artifactId) {
   return sh (script: "curl -s http://search.maven.org/solrsearch/select?q=g:\"${groupId}\"+AND+a:\"${artifactId}\" |grep -Po 'latestVersion.:.\\K[^\"]*'", returnStdout: true).trim()
 }
 
+def getLatestReleaseFromGitHubRepository (owner, repository) {
+  return sh (script: "curl -s curl https://api.github.com/repos/${owner}/${repository}/releases/latest | grep -Po '\"name\"[^\\d]*\\K[\\d\\.]*'", returnStdout: true).trim()
+}
+
 def getXtextTychoVersion (branch) {
-  return sh (script: "curl -s https://raw.githubusercontent.com/eclipse/xtext-eclipse/master/releng/org.eclipse.xtext.tycho.parent/pom.xml |grep -Po '<tycho-version>\\K[^<]*'", returnStdout: true).trim()
+  return sh (script: "curl -s https://raw.githubusercontent.com/eclipse/xtext-eclipse/${branch}/releng/org.eclipse.xtext.tycho.parent/pom.xml |grep -Po '<tycho-version>\\K[^<]*'", returnStdout: true).trim()
 }
 
 def getXtextGradlePluginVersion (branch) {
-  return sh (script: "curl -s https://raw.githubusercontent.com/eclipse/xtext-lib/master/gradle/versions.gradle |grep -Po 'xtext_gradle_plugin[^\\d]*\\K[\\d\\.]*'", returnStdout: true).trim()
+  return sh (script: "curl -s https://raw.githubusercontent.com/eclipse/xtext-lib/${branch}/gradle/versions.gradle |grep -Po 'xtext_gradle_plugin[^\\d]*\\K[\\d\\.]*'", returnStdout: true).trim()
+}
+
+def getXtextGradleVersion (branch) {
+  return sh (script: "curl -s https://raw.githubusercontent.com/eclipse/xtext-lib/${branch}/gradle/wrapper/gradle-wrapper.properties |grep -Po 'distributionUrl=.*/gradle-\\K[\\d\\.]*'", returnStdout: true).trim()
 }
 
 return this
